aa152167b384ea1aa466e53894a0f40ff84137ff
DanielLyubin <dani9590@gmail.com>
2017-05-21 10:10:29 +0300

    working version

--- book-app/src/test/java/il/ac/technion/cs/sd/book/app/PerformanceTestFactory.java
+++ book-app/src/test/java/il/ac/technion/cs/sd/book/app/PerformanceTestFactory.java
@@ -12,87 +12,13 @@ import java.util.*;
  */
 public class PerformanceTestFactory implements LineStorageFactory {
 
-    private Map<Integer, Map<Integer, Integer>> reviewersData, booksData;
-    /*PerformanceTestFactory() {
-        setData();
-    }*/
-    private void setData() {
-        reviewersData = new HashMap<>();
-        booksData = new HashMap<>();
-
-
-        Random random = new Random();
-        int[] bookReviewsHistogram = new int[100000];
-        //This stage sets random data for 1 million reviewers and 100,000 books.
-        for (int id = 1; id <= 1000000; ++id) {
-            Map<Integer, Integer> booksAndGrades = new HashMap<>();
-            for (int i = 0; i < 100; ++i) {
-
-                int bookId = random.nextInt(100000), score = random.nextInt(10)+1;
-                while (bookReviewsHistogram[bookId] == 100)
-                    bookId = random.nextInt(100000);
-
-                booksAndGrades.put(bookId,score);
-                bookReviewsHistogram[bookId]++;
-            }
-
-            reviewersData.put(id, booksAndGrades);
-        }
-        //This stage creates the score list per book id.
-        for(int reviewerId : reviewersData.keySet()) {
-            Map<Integer, Integer> bookScoreMap = reviewersData.get(reviewerId);
-
-            if (bookScoreMap.size() != 100)
-                throw  new RuntimeException("Something went wrong when trying to initialize 100 reviews");
-
-            for (int bookId : bookScoreMap.keySet()) {
-                Map<Integer,Integer> reviewerScoreMap;
-
-                if (booksData.get(bookId) == null)
-                    reviewerScoreMap = new HashMap<>();
-                else
-                    reviewerScoreMap = booksData.get(bookId);
-
-                reviewerScoreMap.put(reviewerId, bookScoreMap.get(bookId));
-                booksData.put(bookId, reviewerScoreMap);
-            }
-        }
-
-        //This stage assures that there are at most 100 reviews per book
-        for (int bookId : booksData.keySet()) {
-            if (booksData.get(bookId).size() > 100)
-                throw  new RuntimeException("Something went wrong when trying to initialize at most 100 reviews per book");
-        }
-    }
-
-    private List<String> createList(Map<Integer, Map<Integer,Integer>> reviewersData, boolean reviewers) {
-        List<String> reviewsList = new ArrayList<>();
-        for (int reviewerId = reviewers ? 1 : 0; reviewerId <= (reviewers ? 1000000 : reviewersData.size()); ++reviewerId) {
-            String reviewerLine = String.valueOf(reviewerId) + " ";
-
-            Optional<Integer> sum = reviewersData.get(reviewerId).values().stream().reduce((o1,o2)->o1+o2);
-
-            if (!sum.isPresent())
-                throw new RuntimeException("Something went wrong when reducing the sum");
-            int average = sum.get() / reviewersData.get(reviewerId).size();
-
-            reviewerLine += String.valueOf(average) + " ";
-
-            List<Integer> keysList = new ArrayList<>(reviewersData.keySet());
-            Collections.sort(keysList);
-
-            for (int i = 0; i < keysList.size(); i++) {
-                int id = keysList.get(i);
-                String bookScoreCouple = String.valueOf(id) + "-" +
-                        String.valueOf(reviewersData.get(reviewerId).get(id));
-                reviewerLine += bookScoreCouple + ((i == keysList.size() - 1) ? "" : ",");
-            }
-            reviewsList.add(reviewerLine);
-        }
-        return reviewsList;
-    }
     @Override
     public LineStorage open(String s) throws IndexOutOfBoundsException {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new IndexOutOfBoundsException("sleeping was interrupted!");
+        }
         LineStorage storage = new StorageForTest();
 
         if (s.equals(BookScoreInitializerImpl.book_filename)) {
